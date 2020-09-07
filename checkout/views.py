48cfa91acb0b6bb54db5e6f7b9c540d48139f8b3
Matthew <matthew.poyner79@gmail.com>
2020-09-07 17:11:53 +1200

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HOZXcBVHRtyW92qfpoFcLGxxhOmyHLfd2VhMBRB0ZP8pQ089p0drHjVqeEBHNPc2mrPxXamgmUgNh41V6szjIHi00PF8DeZx7',
+        'client_secret': 'test_client_secret'
     }
 
     return render(request, template, context)
